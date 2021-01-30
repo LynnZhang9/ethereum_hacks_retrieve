f8863dfa5499d6f7fe9b6760a64872c3b87baad7
LIN <stefanzhang9@outlook.com>
2021-01-30 22:19:57 +0100

    A sketch of phishing network in Ethereum

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ethereum_hacks_retrieve
-Retrieve the hacks records of Ethereum
+## Retrieve the hacks records of Ethereum
+
+* Type: MultiDiGraph
+* Number of nodes: 2973489
+* Number of edges: 13551303
+* Average in degree:   4.5574
+* Average out degree:   4.5574
+* In the dataset folder, the figure "Figure_Phishing.png" indicates the phishing network in Ethereum
--- test_networkx_network.py
+++ test_networkx_network.py
@@ -1,19 +1,33 @@
 import pickle
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 def load_pickle(fname):
     with open(fname, 'rb') as f:
         return pickle.load(f)
 
 
 # G = load_pickle('./MulDiGraph.pkl')
-data_path = r"C:\Users\linzhang2\Documents\Master Thesis\eth_dataset\Xblock\Transaction Dataset\Labeled dataset\Ethereum Phishing Transaction Network\archive\Ethereum Phishing Transaction Network\MulDiGraph.pkl"
+data_path = r'./dataset/archive/EthereumPhishingTransactionNetwork/MulDiGraph.pkl'
+# data_path = r"C:\Users\linzhang2\Documents\Master Thesis\eth_dataset\Xblock\Transaction Dataset\Labeled dataset\Ethereum Phishing Transaction Network\archive\Ethereum Phishing Transaction Network\MulDiGraph.pkl"
 G = load_pickle(data_path)
 print(nx.info(G))
 print('start drawing')
+res = []
+for idx, nd in enumerate(nx.nodes(G)):
+    if G.nodes[nd]['isp'] == 1:
+        # print(nd)
+        # print(G.nodes[nd]['isp'])
+        res.append(nd)
 
+
+
+subG = G.subgraph(res)
+# pos = nx.spring_layout(subG, k=1, scale=5)
+# pos = nx.planar_layout(subG, scale=5)
 plt.subplot(111)
-nx.draw(G)  # networkx draw()
+# nx.draw(subG, pos=pos, node_size=10)  # networkx draw()
+nx.draw(subG, node_size=10)
 plt.show()
 print('done')
 
